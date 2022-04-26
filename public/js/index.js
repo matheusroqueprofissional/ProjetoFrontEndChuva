91ab474cfd511028a2187e6e5acaed5add336f0b
matheusroqueprofissional <matheus.roqueprofissional@gmail.com>
2022-04-26 19:18:56 -0300

    finalizado pagina inicial, iniciando pagina apos clique em botao enviar formulario

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 var coll = document.getElementsByClassName("collapsible");
-
+document.getElementById('conteudo-compartilhe-suas-ideias').style.display='none';
+document.getElementsById("form-envio").style.display="block";
 var i;
 
 for (i = 0; i < coll.length; i++) {
@@ -17,5 +18,23 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+var coll2 = document.getElementsByName("button-criar-topico");
+
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  document.getElementById('link').style.display='block';
+  coll2[i].addEventListener("click", function() {
+    document.getElementById('link').style.display='block';
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      document.getElementById('conteudo-compartilhe-suas-ideias').style.display='none';
+    } 
+  });
+}
 
 
