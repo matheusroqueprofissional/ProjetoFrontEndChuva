4e70f968fdc0039544ec1e4b4c66bbedbc9b1bc1
matheusroqueprofissional <matheus.roqueprofissional@gmail.com>
2022-04-28 09:07:37 -0300

    Correção de bugs no JS e Correção de bugs na image footer

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,12 @@
 var coll = document.getElementsByClassName("collapsible");
-document.getElementById('conteudo-compartilhe-suas-ideias').style.display='block';
+var textarea = document.getElementById("text-area");
+
+
+
+
 document.getElementById('form-envio').style.display='none';
+document.getElementById('conteudo-compartilhe-suas-ideias').style.display='block';
+
 document.getElementById('quadrado-laranja01').style.display='none';
 document.getElementById('quadrado-laranja02').style.display='none';
 document.getElementById('quadrado-laranja03').style.display='none';
@@ -28,32 +34,29 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-var coll2 = document.getElementsByName("button-criar-topico");
-
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  document.getElementById('link').style.display='block';
-  coll2[i].addEventListener("click", function() {
-    document.getElementById('link').style.display='block';
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-      document.getElementById('conteudo-compartilhe-suas-ideias').style.display='none';
-    } 
-  });
-}
-
 
 function mostrar(){
   document.getElementById('form-envio').style.display='block';
   document.getElementById('conteudo-compartilhe-suas-ideias').style.display='none';
 }
 
-function retornar(){
+function criartopico(){
+  
+  document.getElementById('form-envio-sucess').style.display='none';
+  document.getElementById('form-envio').style.display='block';
+  document.getElementById('conteudo-compartilhe-suas-ideias').style.display='none';
+  
+  
+}
+
+function criarnovotopico(){
+  document.getElementById('form-envio-sucess').style.display='none';
+  document.getElementById('form-envio').style.display='block';
+  document.getElementById('conteudo-compartilhe-suas-ideias').style.display='none';
+}
+
+
+function buttonretornar(){
   document.getElementById('form-envio').style.display='none';
   document.getElementById('conteudo-compartilhe-suas-ideias').style.display='block';
 }
@@ -63,6 +66,9 @@ function retornar(){
 
 function clickapresentacao()
 {
+  document.getElementById('form-envio').style.display='none';
+  document.getElementById('conteudo-compartilhe-suas-ideias').style.display='block';
+
   document.getElementById("label-Apresentação").style.backgroundColor="#FDF1EB";
   document.getElementById("label-Comitês").style.backgroundColor="#FFFFFF00";
   document.getElementById("label-Autores").style.backgroundColor="#FFFFFF00";
@@ -78,6 +84,9 @@ function clickapresentacao()
   document.getElementById('quadrado-laranja04').style.display='none';
   document.getElementById('quadrado-laranja05').style.display='none';
   document.getElementById('quadrado-laranja06').style.display='none';
+  
+
+
 }
 function clickcomites()
 {
@@ -159,4 +168,35 @@ function clickcontato()
   document.getElementById('quadrado-laranja05').style.display='none';
   document.getElementById('quadrado-laranja06').style.display='block';
 }
+
+function enviar2()
+{
+  document.getElementById('contentDiscuções').style.height=1800+ 'px';
+  document.getElementById('footer-image').style.display='none';
+  document.getElementById('footer-image1').style.position='relative';
+  document.getElementById('footer-image1').style.top=90+'px';
+var textarea = document.getElementById("text-area");
+var inputassunto=document.getElementById("input-assunto");
+/*
+if(inputassunto.value == "")
+{
+  alert("insira um valor na area de assunto");
+  inputassunto.focus();
+  return;
+}
+
+if(textarea.value == "")
+{
+  alert("insira um valor na area de texto");
+  textarea.focus();
+  return;
+}
+*/
+
+
+  document.getElementById('form-envio').style.display='none';
+  document.getElementById('form-envio-sucess').style.display='block';
+  document.getElementById('conteudo-compartilhe-suas-ideias').style.display='none';
+}
+
 
